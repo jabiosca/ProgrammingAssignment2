ede97a334baa15e6c451432b920d01a132f87b87
jabiosca <josep.biosca@uab.es>
2014-10-25 21:08:29 +0200

    assignment2
    I added a few lines describing the functions makeCacheMatrix and
    cacheSolve that cache the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## solve is a function in R that can be used to calculate the inverse 
+## of a matrix. However, this calculation is computer-intensive, so it is
+## a good strategy to store the inverse of the matrix (and not to calculate
+## it every time) if we need to use it in several parts of the program.
+## The function makeCacheMatrix and cacheSolve can be used to solve the inverse
+## of a matrix and store its value in cache. When this inverse is needed, the
+## functions return its value.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions which allow for the storage
+## of the inverse of a matrix in cache. Matrix x and its inverse, Inv, are defined,
+## so they can be accessed from outside of the function makeCacheMatrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+	Inv<-NULL
+	set<-function(y){
+		x<<-y
+		Inv<<-NULL
+	}
+	get<-function()x
+	setInv<-function(solve) Inv<<-solve
+	getInv<-function() Inv
+	list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
 
+## cacheSolve checks whether the inverse of a requested matrix is stored in cache.
+## If it is, it returns its value. If it is noy, it computes it and then it 
+## calls the setInv() function from the list created by makeCacheMatrix to cache the
+## result. In either case, it returns the matrix's inverse.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+	Inv<-x$getInv()
+	if (!is.null(Inv)){
+		message("getting cached data")
+		return(Inv)
+	}
+	data<-x$get()
+	Inv<-solve(data,...)
+	x$setInv(Inv)
+	Inv
 }
+
